6ed28bd207b32a0ff7e1dabef99bc53d305e3629
piyush <pr16866@gmail.com>
2024-12-28 19:02:34 +0530

    Some chnages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: ['class'],
-    content: [
+export default {
+  darkMode: ['class'],
+  content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',  // Ensure these paths match your file structure
+    './src/**/*.{js,ts,jsx,tsx}', // Ensure these paths match your file structure
   ],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
